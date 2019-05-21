cdf4638e0cade414c8ec8f09612c1e3f7c93c7c7
jiyinyiyong <jiyinyiyong@gmail.com>
2019-05-21 22:58:41 +0800

    upgrade workflow after half a year

---
+++ src/reel/config.cljs
@@ -0,0 +1,25 @@
+
+(ns reel.config )
+
+(def cdn?
+  (cond
+    (exists? js/window) false
+    (exists? js/process) (= "true" js/process.env.cdn)
+    :else false))
+
+(def dev?
+  (let [debug? (do ^boolean js/goog.DEBUG)]
+    (cond
+      (exists? js/window) debug?
+      (exists? js/process) (not= "true" js/process.env.release)
+      :else true)))
+
+(def site
+  {:dev-ui "http://localhost:8100/main-fonts.css",
+   :release-ui "http://cdn.tiye.me/favored-fonts/main-fonts.css",
+   :cdn-url "http://cdn.tiye.me/reel/",
+   :cdn-folder "tiye.me:cdn/reel",
+   :title "Reel",
+   :icon "http://cdn.tiye.me/logo/respo.png",
+   :storage-key "reel",
+   :upload-folder "tiye.me:repo/Respo/reel/"})
